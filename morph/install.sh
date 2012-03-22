e17214566db6f8d3d0f03ed9208eaee099bbc0f6
K900 <k0009000@gmail.com>
2012-03-22 18:35:15 +0400

    Fix bang.

--- morph/install.sh
+++ morph/install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 install -Dm755 morph $PREFIX/usr/bin/morph
 mkdir -p $PREFIX/usr/share/morph
 cp -R {builders,lib,loaders,morphrc,packagers,unpackers} $PREFIX/usr/share/morph
